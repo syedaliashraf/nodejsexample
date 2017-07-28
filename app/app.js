11b55d0e364b470c262387267ad6663f34e9f580
Ali Ashraf <aliashraf@buzzboard.com>
2017-07-28 10:41:34 +0000

    completed simple server initialization and routing

--- app/app.js
+++ app/app.js
@@ -1,10 +1,15 @@
 /*eslint-env node*/
 
+/*eslint-disable no-unused-params */
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/routing1', function (req, res) {
+  res.send('Hello World for routing!');
 });
 
 app.listen(3000, function () {
